a9bc987e998dec2e7f41be52b2c7b49bcc5cbcd2
ko-miyagi <k-miyagi@i-enter.co.jp>
2023-04-21 09:58:18 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 宮城 光太朗 on 2023/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
